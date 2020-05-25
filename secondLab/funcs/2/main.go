08e6df699227d70ae840319f894d5df0a96ff55d
romakv4 <romakv4@gmail.com>
2020-05-25 23:18:11 +0600

    add funcs/3 solve

--- secondLab/funcs/2/main.go
+++ secondLab/funcs/2/main.go
@@ -1,9 +1,13 @@
 package main
 
+import (
+	"reflect"
+)
+
 func showMeTheType(i interface{}) string {
-	return ""
+	return reflect.TypeOf(i).String()
 }
 
 func main() {
-
+	showMeTheType([]int{})
 }
