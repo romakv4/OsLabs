bb87842e4703aa0070949d4082d5c1715795a66e
romakv4 <romakv4@gmail.com>
2020-05-25 22:55:06 +0600

    add funcs/1 solve

--- secondLab/funcs/1/main.go
+++ secondLab/funcs/1/main.go
@@ -2,29 +2,102 @@ package main
 
 import "fmt"
 
-type memoizeFunction func(int, ...int) interface{}
-
-// TODO реализовать
-var fibonacci memoizeFunction
-var romanForDecimal memoizeFunction
-
-//TODO Write memoization function
-
-func memoize(function memoizeFunction) memoizeFunction {
-	return function
+// fibonacci
+func memoize(f func(int) int) func(int) int {
+	cache := make(map[int]int)
+	return func(n int) int {
+		if cache[n] == 0 {
+			cache[n] = f(n)
+		}
+		return cache[n]
+	}
+}
+func fibonacci(n int) int {
+	if n == 1 || n == 2 {
+		return 1
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
 }
 
-// TODO обернуть функции fibonacci и roman в memoize
-func init() {
+//Decimal to roman numbers
+var num = map[string]int{
+	"I": 1,
+	"V": 5,
+	"X": 10,
+	"L": 50,
+	"C": 100,
+	"D": 500,
+	"M": 1000,
+}
+
+var numInv = map[int]string{
+	1000: "M",
+	900:  "CM",
+	500:  "D",
+	400:  "CD",
+	100:  "C",
+	90:   "XC",
+	50:   "L",
+	40:   "XL",
+	10:   "X",
+	9:    "IX",
+	5:    "V",
+	4:    "IV",
+	1:    "I",
+}
+
+var maxTable = []int{
+	1000,
+	900,
+	500,
+	400,
+	100,
+	90,
+	50,
+	40,
+	10,
+	9,
+	5,
+	4,
+	1,
+}
+
+// Roman is roman
+type Roman struct{}
+
+// NewRoman to create a Roman
+func NewRoman() *Roman {
+	return &Roman{}
+}
+
+// ToRoman is to convert decimal number to roman numeral
+func (r *Roman) ToRoman(n int) string {
+	out := ""
+	for n > 0 {
+		v := highestDecimal(n)
+		out += numInv[v]
+		n -= v
+	}
+	return out
+}
+
+func highestDecimal(n int) int {
+	for _, v := range maxTable {
+		if v <= n {
+			return v
+		}
+	}
+	return 1
 }
 
 func main() {
-	fmt.Println("Fibonacci(45) =", fibonacci(45).(int))
+	memoizedFibonacci := memoize(fibonacci)
+	fmt.Println("Fibonacci(45) =", memoizedFibonacci(45))
 	for _, x := range []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
 		14, 15, 16, 17, 18, 19, 20, 25, 30, 40, 50, 60, 69, 70, 80,
 		90, 99, 100, 200, 300, 400, 500, 600, 666, 700, 800, 900,
 		1000, 1009, 1444, 1666, 1945, 1997, 1999, 2000, 2008, 2010,
 		2012, 2500, 3000, 3999} {
-		fmt.Printf("%4d = %s\n", x, romanForDecimal(x).(string))
+		fmt.Printf("%4d = %s\n", x, NewRoman().ToRoman(x))
 	}
 }
